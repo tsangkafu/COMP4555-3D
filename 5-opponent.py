3953158872b06159ec4f61ea4372a10e967a3282
dyank432 <dyank432@mtroyal.ca>
2022-09-26 12:11:50 -0600

    added max ball speed

--- 5-opponent.py
+++ 5-opponent.py
@@ -2,54 +2,54 @@ import pygame
 import sys
 
 
-
 def ball_animation():
 
-  global ball_speed_x, ball_speed_y
+    global ball_speed_x, ball_speed_y
 
+    if ball_speed_x <= 20 or ball_speed_y <= 20:
+        ball_speed_x *= 1.001
+        ball_speed_y *= 1.001
 
-  ball_speed_x *= 1.001
-  ball_speed_y *= 1.001
-  
-  ball.x += ball_speed_x
-  ball.y += ball_speed_y
+    ball.x += ball_speed_x
+    ball.y += ball_speed_y
 
-  # Ball Collision
-  if ball.top <= 0 or ball.bottom >= screen_height:
-    ball_speed_y *= -1
+    # Ball Collision
+    if ball.top <= 0 or ball.bottom >= screen_height:
+        ball_speed_y *= -1
 
-  if ball.left <= 0 or ball.right >= screen_width:
-    ball_speed_x *= -1
+    if ball.left <= 0 or ball.right >= screen_width:
+        ball_speed_x *= -1
 
 
 def player_animation():
 
-  global player_speed
+    global player_speed
 
-  player.y += player_speed
+    player.y += player_speed
 
-  # Player Collision
-  if player.top <= 0:
-    player.top = 0
+    # Player Collision
+    if player.top <= 0:
+        player.top = 0
 
-  if player.bottom >= screen_height:
-    player.bottom = screen_height
+    if player.bottom >= screen_height:
+        player.bottom = screen_height
+
 
 def opponent_ai():
-  
-  global opponent_speed
 
-  if opponent.top < ball.top : #opponent is above ball
-    opponent.y += opponent_speed
+    global opponent_speed
 
-  if opponent.bottom > ball.bottom: # opponent is below ball
-    opponent.y -= opponent_speed
+    if opponent.top < ball.top:  # opponent is above ball
+        opponent.y += opponent_speed
 
-  if opponent.top <= 0:
-    opponent.top = 0
+    if opponent.bottom > ball.bottom:  # opponent is below ball
+        opponent.y -= opponent_speed
 
-  if opponent.bottom >= screen_height:
-    opponent.bottom = screen_height
+    if opponent.top <= 0:
+        opponent.top = 0
+
+    if opponent.bottom >= screen_height:
+        opponent.bottom = screen_height
 
 
 pygame.init()
@@ -62,7 +62,7 @@ pygame.display.set_caption('Pong')
 
 # Game Rectangle
 ball = pygame.Rect(screen_width/2-15, screen_height/2-15, 30, 30)
-player = pygame.Rect(screen_width-20, screen_height/ \
+player = pygame.Rect(screen_width-20, screen_height /
                      2-70, 10, 140)  # -70 missing
 opponent = pygame.Rect(10, screen_height/2-70, 10, 140)  # -70 missing
 
@@ -84,17 +84,17 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-        
+
         if event.type == pygame.KEYDOWN:
-          if event.key == pygame.K_UP:
-            player_speed -= 6
-          if event.key == pygame.K_DOWN:
-            player_speed += 6
+            if event.key == pygame.K_UP:
+                player_speed -= 6
+            if event.key == pygame.K_DOWN:
+                player_speed += 6
         if event.type == pygame.KEYUP:
-          if event.key == pygame.K_UP:
-            player_speed += 6
-          if event.key == pygame.K_DOWN:
-            player_speed -= 6
+            if event.key == pygame.K_UP:
+                player_speed += 6
+            if event.key == pygame.K_DOWN:
+                player_speed -= 6
 
     ball_animation()
     player_animation()
